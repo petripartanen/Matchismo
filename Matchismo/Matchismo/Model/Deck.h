13aaf3119b7175f17a406f48520daa7fce938865
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-01 20:19:23 +0200

    Added base class Deck under Models group

---
+++ Matchismo/Matchismo/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Petri Partanen on 01/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Petri Partanen on 01/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
