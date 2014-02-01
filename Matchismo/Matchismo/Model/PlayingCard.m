0ce1618c4ab69ea3a64da60512b50b48f474c770
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-01 21:00:51 +0200

    Added a new class PlayingCard : Card under Model group

---
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by Petri Partanen on 01/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
