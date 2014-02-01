68b84303ddd54d6821c0411b88b0b8f14cae4f9a
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-01 20:43:25 +0200

    Deck is now able to store Cards

--- Matchismo/Matchismo/Model/Deck.m
+++ Matchismo/Matchismo/Model/Deck.m
@@ -8,6 +8,35 @@
 
 #import "Deck.h"
 
+@interface Deck()
+
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+
+@end
+
 @implementation Deck
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) {
+        _cards = [[NSMutableArray alloc] init];
+    }
+    
+    return _cards;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else  {
+        [self.cards addObject:card];
+    }
+}
+
+- (void)addCard:(Card *)card
+{
+    [self addCard:card atTop:NO];
+}
 
 @end
