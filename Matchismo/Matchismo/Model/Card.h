31cad87f92cbac3c1285e24bbfc229f8fda45e4d
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-01 20:03:37 +0200

    Added Card base class and set it under new Model group.

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Petri Partanen on 01/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Petri Partanen on 01/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
