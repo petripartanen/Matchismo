f19a3880d97b2fbf2839b53243aaeb515d587231
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 13:32:11 +0200

    PlayingCardDeck will now initialise itself with PlayingCards

--- Matchismo/Matchismo/Model/PlayingCardDeck.m
+++ Matchismo/Matchismo/Model/PlayingCardDeck.m
@@ -7,7 +7,26 @@
 //
 
 #import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @implementation PlayingCardDeck
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *playingCard = [[PlayingCard alloc]init];
+                playingCard.rank = rank;
+                playingCard.suit = suit;
+                [self addCard:playingCard];
+            }
+        }
+    }
+    
+    return self;
+}
 
 @end
