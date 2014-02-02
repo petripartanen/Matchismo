5e2fe5b68f935c04c965f791c455bace0d70fe0b
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 16:53:13 +0200

    Card now has properties matched and chosen.

--- Matchismo/Matchismo/Model/Card.h
+++ Matchismo/Matchismo/Model/Card.h
@@ -11,6 +11,8 @@
 @interface Card : NSObject
 
 @property (strong, nonatomic) NSString *contents;
+@property (nonatomic, getter = isChosen) BOOL chosen;
+@property (nonatomic, getter = isMatched) BOOL matched;
 
 - (int)match:(NSArray *)otherCards;
 
