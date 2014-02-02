4d9f23501062b8a289a7eec705fc630dea33d7e8
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 21:15:26 +0200

    Mismatch and choosing a card now causes small penalty

--- Matchismo/Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -58,6 +58,9 @@
     return (index < [self.cards count]) ? self.cards[index] : Nil;
 }
 
+static const int MISMATCH_PENALTY = 2;
+static const int COST_TO_CHOOSE = 1;
+
 - (void)chooseCardAtIndex:(NSUInteger)index
 {
     Card *card = [self cardAtIndex:index];
@@ -75,13 +78,14 @@
                         otherCard.matched = YES;
                         card.matched = YES;
                     } else {
+                        self.score -= MISMATCH_PENALTY;
                         otherCard.chosen = NO;
                     }
                     
                     break;
                 }
             }
-            
+            self.score -= COST_TO_CHOOSE;
             card.chosen = YES;
         }
     }
