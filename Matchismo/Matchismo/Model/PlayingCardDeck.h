182f0211ea64544b50d19a1b735ee06789859aee
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 13:18:34 +0200

    Added a new PlayingCardDeck : Deck class

---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Petri Partanen on 02/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Petri Partanen on 02/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
