090f6946e1ec4fd25fdba551556adde79e7ad8a6
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 16:14:31 +0200

    Added a new class CardMatchingGame

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Petri Partanen on 02/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Petri Partanen on 02/02/14.
+//  Copyright (c) 2014 Petri Partanen. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
