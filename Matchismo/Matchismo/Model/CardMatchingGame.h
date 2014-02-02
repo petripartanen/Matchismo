bae0543a6c930bbcd6bd80225c65b843a35aaee3
Petri Partanen <petri.j.partanen@metropolia.fi>
2014-02-02 16:46:07 +0200

    CardMatchingGame now   has designated initialiser that will initialise game with given number of cards from given deck.   has score to indicate player's score   has cardAtIndex method to retrieve a card

--- Matchismo/Matchismo/Model/CardMatchingGame.h
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -7,7 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Deck.h"
 
 @interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (readonly, nonatomic) NSInteger score;
 
 @end
--- Matchismo/Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -8,6 +8,54 @@
 
 #import "CardMatchingGame.h"
 
+@interface CardMatchingGame()
+
+@property (readwrite, nonatomic) NSInteger score;
+@property (strong, nonatomic) NSMutableArray *cards; // of Card
+
+@end
+
 @implementation CardMatchingGame
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) {
+        _cards = [[NSMutableArray alloc]init];
+    }
+    
+    return _cards;
+}
+
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck
+{
+    if (count < 2) {
+        // We need at least two cards to play card matching game
+        return Nil;
+    }
+    
+    self = [super init];
+    
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *card = [deck drawRandomCard];
+            
+            if (card) {
+                [self.cards addObject:card];
+            } else {
+                // Deck didn't have enough cards; initialization failed
+                self = Nil;
+                break;
+            }
+            
+        }
+    }
+    
+    return self;
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index
+{
+    return (index < [self.cards count]) ? self.cards[index] : Nil;
+}
 
 @end
